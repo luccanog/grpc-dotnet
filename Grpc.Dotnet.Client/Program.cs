3e0e45157e3cdeda7900c3b00ac32505a8763905
luccanog <luccathomaz@gmail.com>
2023-07-26 00:23:10 -0300

    working client - server communication

--- Grpc.Dotnet.Client/Program.cs
+++ Grpc.Dotnet.Client/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Grpc.Dotnet.Client;
+using Grpc.Net.Client;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7256");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
